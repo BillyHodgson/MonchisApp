35b6e6438bb6e31218c70e931fb5507241e2c16f
estebanqj84 <estebanqj1984@gmail.com>
2021-07-28 14:54:41 -0600

    Creacion de ProductosEntity, CategoriaEntity

---
+++ Entity/dbo/Productos/ProductosEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+   public class ProductosEntity: EN
+    {
+        public int? IdProducto { get; set; }
+
+        public int? IdCategoria { get; set; }
+
+        public string  Nombre { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public string Caracteristicas { get; set; }
+
+        
+    }
+}
