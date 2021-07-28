ade3e16df4bed894ffdf0c7b8986558ed4b50ecb
Billy Hodgson <billyhodgson@hotmail.com>
2021-07-27 22:17:11 -0600

    Add project files.

---
+++ BaseDatos/dbo/Tables/Pedidos/Pedido.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Pedido]
+(  
+    IdPedido INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Pedido PRIMARY KEY CLUSTERED(IdPedido)
+  , Cliente VARCHAR(250) NOT NULL /*Debe venir de Clientes*/
+  , FechaPedido DATE NOT NULL
+  , Producto VARCHAR(250) NOT NULL /*Debe venir de Productos*/
+  , Cantidad INT NOT NULL /*Debe validar si esta disponible la cantidad*/
+  , SubTotal INT NOT NULL 
+  , SubEnvio INT NOT NULL
+  , IVA INT NOT NULL 
+  , Total INT NOT NULL 
+  
+)
+WITH (DATA_COMPRESSION=PAGE)
+GO
+
+/*CREATE UNIQUE NONCLUSTERED INDEX IDX_Persona_Cedula
+ON dbo.Persona(Cedula)
+WITH (DATA_COMPRESSION=PAGE)
+GO*/
